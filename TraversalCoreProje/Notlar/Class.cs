0a8914e22c117c393625ad97812a913dae043680
Goksuudgn <goksuudogan@gmail.com>
2024-12-15 02:12:43 +0300

    Add project files.

---
+++ TraversalCoreProje/Notlar/Class.cs
@@ -0,0 +1,13 @@
+﻿namespace TraversalCoreProje.Notlar
+{
+    public class Class
+    {
+        //N katmanlı mimari (N Tier Arc.)
+        // Code First
+        //Entity Data Business Presentation
+        //Entitiy = tale=>Class Column=>Property
+        //Data = Veri tabanı crud işlemleri (repository Deign Pattern)
+        //Business = Validasyonların kontrolü (Mesela lokasyon max 50 karakter olmalı veya fiyat sayısal veri olmalı yani uyumsuzlukların önüne geçmek crud işlemlerini sorgulamak için yapılır
+        //Presentation = sunum katmanı controller tarafındaki backend kodları ve projenin UI kısmı bu katmanda yer alır 
+    }
+}
